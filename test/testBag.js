9d2e0efddd9b77b5607ec493dd367fdaf1fa4ee1
John Bussey <jbussey365@yahoo.com>
2020-07-05 10:18:10 -0400

    added test to ensure random draws are different

--- test/testBag.js
+++ test/testBag.js
@@ -1,20 +1,22 @@
 var assert = require('assert');
 var Bag = require('../src/components/Bag.js');
+var bag;
 
-var mockTiles = [1,2,3,4,5,6,7,8,9,10];
-
-var bag = new Bag(mockTiles);
-
+beforeEach(() => {
+  bag = new Bag([1,2,3,4,5,6,7,8,9,10]);
+});
 
 describe('Bag', function () {
 
   describe('drawTiles()', function () {
+
     it('should return an array', function () {
       assert(Array.isArray(bag.drawTiles()));
     });
 
     it('should return array length 3 when called with arg 3', function () {
-      assert.equal(bag.drawTiles(3).length, 3);
+      var drawnTiles = bag.drawTiles(3);
+      assert.equal(drawnTiles.length, 3);
     });
 
     it('should return empty array when called with no args', function () {
@@ -23,6 +25,20 @@ describe('Bag', function () {
 
     it('should throw exception when we try to draw more than it has', function () {
       assert.throws(function(){bag.drawTiles(1000)}, Error);
+    });
+
+    it('should draw different tiles at random', function () {
+      var different = false;
+      var lastDraw = null;
+      for(let i = 0; i < 10; i++){
+
+        var thisDraw = bag.drawTile();
+        if(i>0 && lastDraw !== thisDraw){
+          different = true;
+        }
+        lastDraw = thisDraw;
+      }
+      assert(different);
     });
 
 
