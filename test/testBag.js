1905cc96506ec09b5553cba3e6043ae44d218bb6
John Bussey <jbussey365@yahoo.com>
2020-07-05 10:49:56 -0400

    finished tests for Bag class

--- test/testBag.js
+++ test/testBag.js
@@ -8,6 +8,22 @@ beforeEach(() => {
 
 describe('Bag', function () {
 
+  describe('drawTile()', function () {
+    it('should draw different tiles at random', function () {
+      var different = false;
+      var lastDraw = null;
+      for(let i = 0; i < 10; i++){
+
+        var thisDraw = bag.drawTile();
+        if(i>0 && lastDraw !== thisDraw){
+          different = true;
+        }
+        lastDraw = thisDraw;
+      }
+      assert(different);
+    });
+  });
+
   describe('drawTiles()', function () {
 
     it('should return an array', function () {
@@ -26,21 +42,34 @@ describe('Bag', function () {
     it('should throw exception when we try to draw more than it has', function () {
       assert.throws(function(){bag.drawTiles(1000)}, Error);
     });
+  });
 
-    it('should draw different tiles at random', function () {
-      var different = false;
-      var lastDraw = null;
-      for(let i = 0; i < 10; i++){
+  describe('tradeTiles()', function () {
 
-        var thisDraw = bag.drawTile();
-        if(i>0 && lastDraw !== thisDraw){
-          different = true;
-        }
-        lastDraw = thisDraw;
-      }
-      assert(different);
+    it('should give back as many as you submit', function () {
+
+      var input = [45,46,47]
+      var output = bag.tradeTiles(input);
+
+      assert.equal(input.length, output.length);
     });
 
+    it('should give back none of the tiles that were submitted', function () {
 
+      var originalContents = [1,2,3];
+      var smallBag = new Bag(originalContents);
+
+      var input = [4,5,6];
+      var output = smallBag.tradeTiles(input);
+
+      var overlap = input.filter(value => output.includes(value))
+
+      assert.equal(overlap.length, 0);
+    });
+
+    it('should throw exception when we try to trade more than it has', function () {
+      var input = [1,2,3,4,5,6,7,8,9,10,11];
+      assert.throws(function(){bag.tradeTiles(input)}, Error);
+    });
   });
 });
